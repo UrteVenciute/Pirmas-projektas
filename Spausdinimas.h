e378b7279abdb5b2e194a6004bb90abee18a401e
UrteVenciute <90273563+UrteVenciute@users.noreply.github.com>
2021-12-15 18:51:06 +0200

    Delete Spausdinimas.h

--- Spausdinimas.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "Funkcijos.h"
-#include "Studentas.h"
-
-void spausdinimas(int& v1, char& atsakymas, vector <studentas>& sarasas, string& pav);
-void spausdinimas(int& v1, char& atsakymas, list <studentas>& sarasas, string& pav);
